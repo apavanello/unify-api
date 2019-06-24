ef22a2e6805a799cb8bede52d06a9b2f05a0abd1
alexandre pavanello e silva <apavanello@live.com>
2019-06-24 12:20:55 +0000

    Revert "Merge branch 'Cors' into 'master'"
    This reverts merge request !3

--- src/app.py
+++ src/app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from flask import request
 from src.classes import DefenceCode
-from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/v1/*": {"origins": "*"}})
 
 
 @app.route('/<version>/<path:extra_args>', methods=['GET', 'POST'])
@@ -12,7 +10,8 @@ def api_selector(version, extra_args):
 
     df = DefenceCode()
     response = df.checks(version, extra_args, request.method)
-    return '%s' % response
+#   response = DefenceCode.checks(version=version, extra_args=extra_args, request_method=request.method)
+    return '<h2> %s </h2>' % response
 
 
 if __name__ == '__main__':
